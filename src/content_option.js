13717527511018251f03e0e249c17c2fc1d2b658
user <sunday.peter@decagon.dev>
2023-12-01 07:07:47 +0100

    initial

---
+++ src/content_option.js
@@ -0,0 +1,146 @@
+const logotext = "SUNDAY PETER OCHEFIJE";
+const meta = {
+    title: "Sunday Peter",
+    description: "I’m Sunday Peter, a Java Developer ",
+};
+
+const introdata = {
+    title: "I’m Sunday Peter",
+    animated: {
+        first: "I love coding",
+        second: "I code Java/SpringBoot",
+        third: "I develop integrated and secure backend System",
+        fourth: "I am the one you for the Job,"
+    },
+    description: "",
+    your_img_url: "https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/365815778_5779209855513939_2821730805622339031_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=yi_ji-lcQhQAX_VDamY&_nc_ht=scontent-los2-1.xx&oh=00_AfDdSWL9hy29tN6SydgsSj1oQpwuVSrCl8BN8ivO58s5zw&oe=656D30E1",
+};
+
+const dataabout = {
+    title: "SUMMARY",
+    aboutme: "A passionate and outstanding developer, who is adept in bringing forth expertise in designing, installing, testing, and maintaining software systems. \n Skilled at writing high-quality codes for high-paced businesses, in line with the latest technological advancements. Proficient in various technologies, including Java, Spring Boot, Building Micro services, JPA, MySQL, Postgres, Entity manager, Git workflow and other latest cutting-edge and secured development tools and procedures. Able to effectively self-manage during independent projects and collaborate as part of a productive team. Deeply knowledgeable in Data Structure, Software Architecture, Test Driven Development, and Agile methodology, CI/CD, with demonstrated expertise in all phases of a project including Design, Development, Debugging/Testing, and Deployment. Seeking opportunities to create something beautiful with people who bring out the best in me.",
+};
+const worktimeline = [{
+        jobtitle: "REMOTE",
+        where: "",
+        date: "GLOBAL",
+    },
+    {
+        jobtitle: "HYBRID",
+        where: "",
+        date: "GLOBAL",
+    },
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "ALquds",
+    //     date: "2019",
+    // },
+];
+
+const skills = [{
+        name: "JAVA",
+        value: 100,
+    },
+    {
+        name: "SPRING BOOT",
+        value: 100,
+    },
+    {
+        name: "MYSQL / POSTGRES",
+        value: 100,
+    },
+    {
+        name: "AWS/AZURE/GCP",
+        value: 100,
+    },
+    {
+        name: "GIT, CI/CD",
+        value: 100,
+    },
+    {
+        name: "KAFKA ",
+        value: 100,
+    },
+    {
+        name: "TDD",
+        value: 100,
+    },
+    {
+        name: "SWAGGER/POSTMAN",
+        value: 100,
+    },
+];
+
+const services = [{
+        title: "Backend Developer",
+        description: "As an accomplished Backend Developer specializing in Java, Spring Boot, and microservices architecture, I excel in orchestrating the core functionality of digital ecosystems. With a precise blend of innovation, I design server-side solutions for high-performance applications.\n\nMy expertise in Java and Spring Boot allows me to create scalable backend solutions, including the development of RESTful APIs and the design of flexible microservices architectures. I adeptly manage relational databases like MySQL and PostgreSQL for data integrity and efficiency, while also utilizing NoSQL databases like MongoDB for diverse data structures.\n\nCollaboration is integral to my approach, working seamlessly with DevOps teams to ensure continuous integration and deployment. I am proficient in troubleshooting bottlenecks and implementing new features with a focus on performance and scalability.\n\nCommitted to staying current with industry best practices, I stay informed about security standards and the latest advancements in backend technologies."
+    },
+    {
+        title: "Mobile Apps",
+        description: "As a skilled Mobile Developer specializing in Flutter, I excel in creating cross-platform applications that seamlessly blend functionality. From design to implementation, I ensure a superior user experience, translating ideas into polished mobile solutions.\n\nDedicated to staying current with industry trends, innovative applications. "
+    },
+    // {
+    //     title: "",
+    //     description: "",
+    // },
+];
+
+const dataportfolio = [{
+        img: "https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/407462985_6182605395174381_3220541341405342605_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=3635dc&_nc_ohc=xG5yu6jrSfkAX8QRGp_&_nc_ht=scontent-los2-1.xx&oh=00_AfAVUqq_pRtXlKnmbWKEM3sc9sLrRQAhJKl4afa2n61dRA&oe=656E920C",
+        description: "Logistic App with Java as backend and Flutter for Mobile",
+        link: "https://play.google.com/store/apps/details?id=ng.ridex.user  ",
+    },
+    {
+        img: "https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/406651514_6182681931833394_4760269935540393507_n.jpg?stp=dst-jpg_p180x540&_nc_cat=103&ccb=1-7&_nc_sid=3635dc&_nc_ohc=5NAkroLIXzQAX8FC5bW&_nc_ht=scontent-los2-1.xx&oh=00_AfB4IpUFMaw-kzH0LQZJFm02YcqxS1NWX7xGuJzfLLI6tg&oe=656E855C",
+        description: "Swift Select e-Hiring with Java and React",
+        link: "#",
+    },
+    {
+        img: "https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/399336547_6182680675166853_6560042604224829015_n.jpg?stp=dst-jpg_p180x540&_nc_cat=107&ccb=1-7&_nc_sid=3635dc&_nc_ohc=26BFx6kkFn0AX_l9z7F&_nc_ht=scontent-los2-1.xx&oh=00_AfCmM3HZB-C2MP-F0SJ3BXusDOmyR1NuHeolBHiCF0CD6w&oe=656DDA01",
+        description:  "Swift Select e-Hiring with Java and React",
+        link: "#",
+
+    },
+    {
+        img: "        https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/407373278_6182627215172199_105655842733005210_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=3635dc&_nc_ohc=ifpR8GUPFEwAX-ik7lH&_nc_ht=scontent-los2-1.xx&oh=00_AfCAGBZUV3PquB2_nqPlJyduoyoS1AEFkXayjhq-RcevmQ&oe=656D47A1",
+        description: "Alpha Bank Fintech App with Java/React/flutter ",
+        link: "#",
+    },
+    {
+        img: "  https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/407372998_6182671415167779_3563559725909258730_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=3635dc&_nc_ohc=CQBpJcM2fOgAX9Txx2d&_nc_ht=scontent-los2-1.xx&oh=00_AfAurJo3M7gW22aqhHUYrfYLe8lZ8ZGzyfiS9blKtgQZiQ&oe=656EEEC9      ",
+        description: "MobilFind core Java and Kotlin(mobile)",
+        link: "https://play.google.com/store/apps/details?id=com.decagon.mobifind",
+    },
+
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "sundaypetersp12@gmail.com",
+    YOUR_FONE: "+2348186707807",
+    description: "Thank you for taking your time to contact me, I will do well to respond as fast as possible. Please know that the number +2348186707807 is my whatsapp handle and i treat that too as official ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_77blnfx",
+    YOUR_TEMPLATE_ID: "template_uj0h72g",
+    YOUR_USER_ID: "rK8b_mSfYqNt-bVf4",
+};
+
+const socialprofils = {
+    github: "https://github.com/code-with-sunday",
+    facebook: "https://www.facebook.com/SundayP4CHRIST/",
+    linkedin: "https://www.linkedin.com/in/sunday-peter-b74ab3198/",
+    twitter: "https://twitter.com/Zeussunday1",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
